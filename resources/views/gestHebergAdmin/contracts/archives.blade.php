f6c2a2a3274d196a3870db5a9f52944b2c22ae25
root <root@Laravel>
2018-03-23 14:02:29 +0100

    GestHeberg - Writing - INDEV0.1.06
    Modified : Added form and list

--- resources/views/gestHebergAdmin/contracts/archives.blade.php
+++ resources/views/gestHebergAdmin/contracts/archives.blade.php
@@ -5,5 +5,40 @@
 @endsection
 
 @section('content')
-<h1>Test 2</h1>
+
+  <h1>Archive des contrats</h1>
+  {{-- <section class="row text-center placeholders">
+    <div class="col-6 col-sm-3 placeholder">
+      <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+      <h4>Label</h4>
+      <div class="text-muted">Something else</div>
+    </div>
+  </section> --}}
+  <div class="table-responsive">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>N°</th>
+          <th>Client</th>
+          <th>Début</th>
+          <th>Fin</th>
+          <th>Prix</th>
+          <th>Type</th>
+        </tr>
+      </thead>
+      <tbody>
+      	{{-- @foreach --}}
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        {{-- @endforeach --}}
+      </tbody>
+    </table>
+  </div>
+</main>
 @endsection
--- resources/views/gestHebergAdmin/contracts/newType.blade.php
+++ resources/views/gestHebergAdmin/contracts/newType.blade.php
@@ -6,4 +6,14 @@
 
 @section('content')
 <h1>Test 2</h1>
+<form class="" method="POST" action=""> 
+	<label>Nom :</label>
+	<input type="" name="name of the new contract type">
+	<hr>
+	<label>Description :</label>
+	<br>
+	<textarea name='libelle'></textarea>
+	<hr>
+	<button class="submit">Valider</button>
+</form>
 @endsection
